b0d0303bc8bff668f143d28ef287072146edcb87
Elle K <elle@linuxacademy.com>
2020-05-27 11:18:51 -0400

    spelling

--- labfiles/bash/init.sh
+++ labfiles/bash/init.sh
@@ -3,5 +3,5 @@
 sudo hostnamectl set-hostname $HOSTNAME
 sudo apt install apache2 php7.4 mysql-server-8.0 -y
 sudo systemctl start apache2 mysql
-sudo systemctl enable apahe2 mysql
+sudo systemctl enable apache2 mysql
 sudo a2dissite 000-default.conf
