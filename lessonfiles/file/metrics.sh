4dae458a7026424cef838e644d5b8dc9954d41c3
Elle <elle@linuxacademy.com>
2020-05-28 09:40:19 -0400

    Enable Node Exporter upon image start

--- lessonfiles/file/metrics.sh
+++ lessonfiles/file/metrics.sh
@@ -6,5 +6,6 @@ sudo mv /home/ubuntu/node_exporter-1.0.0.linux-amd64/node_exporter /usr/local/bi
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 sudo mv /tmp/node_exporter.service /etc/systemd/system/node_exporter.service
 sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
 rm /tmp/node-exporter.tar.gz
 rm -rf /home/ubuntu/node_exporter-1.0.0.linux-amd64/
