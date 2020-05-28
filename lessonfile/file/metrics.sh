b7cbd046c0d9db7aaf758147017cd0f8145d0346
Elle <elle@linuxacademy.com>
2020-05-28 09:05:57 -0400

    Service file and updated metrics script

--- lessonfile/file/metrics.sh
+++ lessonfile/file/metrics.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 sudo useradd --no-create-home --shell /bin/false node_exporter
-tar -xvf /tmp/node-exporter.tar.gz
-sudo mv /tmp/node-exporter/node_exporter /usr/local/bin/
+tar -xvf /tmp/node-exporter.tar.gz 
+sudo mv /tmp/node_exporter-1.0.0.linux-amd64/node_exporter /usr/local/bin/
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 sudo systemctl daemon-reload
 sudo systemctl start node_exporter
