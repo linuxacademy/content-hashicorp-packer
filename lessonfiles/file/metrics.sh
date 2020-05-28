dcea5057c45356534fc9d9c3e6accbdf1ebff375
Elle <elle@linuxacademy.com>
2020-05-28 09:25:21 -0400

    Added file cleanup to metrics script

--- lessonfiles/file/metrics.sh
+++ lessonfiles/file/metrics.sh
@@ -2,7 +2,9 @@
 
 sudo useradd --no-create-home --shell /bin/false node_exporter
 tar -xvf /tmp/node-exporter.tar.gz 
-sudo mv /tmp/node_exporter-1.0.0.linux-amd64/node_exporter /usr/local/bin/
+sudo mv /home/ubuntu/node_exporter-1.0.0.linux-amd64/node_exporter /usr/local/bin/
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
-sudo systemctl daemon-reload
+sudo mv /tmp/node_exporter.service /etc/systemd/system/node_exporter.service
 sudo systemctl start node_exporter
+rm /tmp/node-exporter.tar.gz
+rm -rf /home/ubuntu/node_exporter-1.0.0.linux-amd64/
