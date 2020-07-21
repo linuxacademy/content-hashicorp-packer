97b0794368eb7a2da425d24a9c791c1d9fe482d2
Elle <elle@linuxacademy.com>
2020-07-21 12:40:06 -0400

    Salt lesson files download script

--- lessonfiles/salt/script.sh
+++ lessonfiles/salt/script.sh
@@ -1,1 +1,8 @@
 mkdir salt/apache
+curl https://raw.githubusercontent.com/linuxacademy/content-hashicorp-packer/master/lessonfiles/salt/apache/init.sls -o salt/apache/init.sls
+curl https://raw.githubusercontent.com/linuxacademy/content-hashicorp-packer/master/lessonfiles/salt/apache/install.sls -o salt/apache/install.sls
+curl https://raw.githubusercontent.com/linuxacademy/content-hashicorp-packer/master/lessonfiles/salt/apache/service.sls -o salt/apache/service.sls
+curl https://raw.githubusercontent.com/linuxacademy/content-hashicorp-packer/master/lessonfiles/salt/apache/vhosts.sls -o salt/apache/vhosts.sls
+curl https://raw.githubusercontent.com/linuxacademy/content-hashicorp-packer/master/lessonfiles/salt/top.sls -o salt/top.sls
+curl https://raw.githubusercontent.com/linuxacademy/content-hashicorp-packer/master/lessonfiles/salt/pillar/apache.sls -o pillar/apache.sls
+curl https://raw.githubusercontent.com/linuxacademy/content-hashicorp-packer/master/lessonfiles/salt/pillar/top.sls -o pillar/top.sls
