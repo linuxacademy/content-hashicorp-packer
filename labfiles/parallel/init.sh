84574e60d05aa4456d73b460917d829c01e0d8e4
Elle <elle@linuxacademy.com>
2020-07-29 12:27:47 -0400

    Parallel builds lab files

---
+++ labfiles/parallel/init.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+SUDO=''
+if (( $EUID != 0 )); then
+    SUDO='sudo'
+fi
+
+$SUDO hostnamectl set-hostname $HOSTNAME
+$SUDO apt update && $SUDO apt upgrade -y
+$SUDO apt install apache2 
+$SUDO mkdir -p /var/www/html/bookapp
+$SUDO mv /tmp/bookapp/* /var/www/html/bookapp/
+$SUDO mv /tmp/bookapp.conf /etc/apache2/sites-available/
+$SUDO chown -R www-data:www-data /var/www/html/bookapp
+$SUDO a2dissite 000-default.conf
+$SUDO a2ensite bookapp.conf
+$SUDO systemctl start apache2
+$SUDO systemctl enable apache2
