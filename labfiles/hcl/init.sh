8ee9d84eece539dbb380677b85f872e1785595d8
Elle <elle@linuxacademy.com>
2020-08-03 17:02:52 -0400

    HCL lesson and lab

---
+++ labfiles/hcl/init.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+SUDO=''
+if (( $EUID != 0 )); then
+    SUDO='sudo'
+fi
+
+$SUDO apt update && $SUDO apt upgrade -y
+$SUDO apt install apache2 -y
+$SUDO mkdir -p /var/www/html/bookapp
+$SUDO mv /tmp/bookapp/* /var/www/html/bookapp/
+$SUDO mv /tmp/bookapp.conf /etc/apache2/sites-available/
+$SUDO chown -R www-data:www-data /var/www/html/bookapp
+$SUDO a2dissite 000-default.conf
+$SUDO a2ensite bookapp.conf
